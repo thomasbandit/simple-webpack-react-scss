b3b7db66754b5e1c873d432f8a263c3054b3fd91
Thomas Mills <mills.thoma@gmail.com>
2018-07-18 22:37:43 +0200

    react-lloadable added

--- dist/styles.js
+++ dist/styles.js
@@ -1,2 +1,2 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[0],{56:function(n,o,c){},61:function(n,o,c){},64:function(n,o,c){},71:function(n,o,c){}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[0],{58:function(n,o,c){},62:function(n,o,c){},65:function(n,o,c){},72:function(n,o,c){}}]);
 //# sourceMappingURL=styles.js.map
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Loadable from 'react-loadable';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router';
-import {
-  About,
-  Home,
-  NoMatch,
-  UserProfile,
-} from '../pages';
+import { Home, NoMatch } from '../pages';
 import { Navigation } from '../components';
 import styles from '../assets/scss/main.scss';
+
+/* eslint-disable */
+function Loading({ error }) {
+  if (error) {
+    return 'Oh nooess!';
+  }
+
+  return <h3>Loading...</h3>;
+}
+
+const About = Loadable({
+  loader: () => import('../pages/About'),
+  loading: Loading,
+});
+
+const UserProfile = Loadable({
+  loader: () => import('../pages/UserProfile'),
+  loading: Loading,
+});
 
 const AppContainer = ({ pathname }) => (
   <div>
